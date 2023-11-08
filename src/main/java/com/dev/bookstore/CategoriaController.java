851bb9b01186540617707230632f9c51b27b8d37
rsarocha80 <rodrigosantosrocha80@gmail.com>
2023-11-08 07:28:56 -0300

    feat: inserido endpoint para salvar e atualizar

--- src/main/java/com/dev/bookstore/CategoriaController.java
+++ src/main/java/com/dev/bookstore/CategoriaController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.bookstore.domain.dto.CategoriaDTO;
 import com.dev.bookstore.domain.entity.Categoria;
+import com.dev.bookstore.domain.vo.CategoriaVO;
 import com.dev.bookstore.serveci.CategoriaService;
 
 @RestController
@@ -20,16 +24,28 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaService categoriaService;
 
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Long id) {
-		Categoria cat = categoriaService.findById(id);
-		return ResponseEntity.ok().body(cat);
+	@PostMapping
+	public ResponseEntity<CategoriaDTO> create(@RequestBody final CategoriaVO vo) {
+		CategoriaDTO create = categoriaService.create(vo);
+		return ResponseEntity.ok().body(create);
+	}
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable final Long id, @RequestBody final CategoriaVO vo) {
+		CategoriaDTO update = categoriaService.update(id, vo);
+		return ResponseEntity.ok().body(update);
 	}
 
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<CategoriaDTO> cat = categoriaService.findAll();
+		return ResponseEntity.ok().body(cat);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Long id) {
+		Categoria cat = categoriaService.findById(id);
 		return ResponseEntity.ok().body(cat);
 
 	}
--- src/main/java/com/dev/bookstore/serveci/CategoriaService.java
+++ src/main/java/com/dev/bookstore/serveci/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.bookstore.domain.dto.CategoriaDTO;
 import com.dev.bookstore.domain.entity.Categoria;
+import com.dev.bookstore.domain.vo.CategoriaVO;
 import com.dev.bookstore.exception.ObjectNotFoundException;
 import com.dev.bookstore.repository.CategoriaRepository;
 
@@ -19,13 +20,19 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
-	@Transactional(readOnly = true)
-	public Categoria findById(Long id) {
-		Optional<Categoria> obj = categoriaRepository.findById(id);
-		Categoria cat = obj//
-				.orElseThrow(() -> new ObjectNotFoundException(
-						"Objeto não encontrado! id: " + id + ", tipo " + Categoria.class.getName()));
-		return cat;
+	@Transactional
+	public CategoriaDTO create(final CategoriaVO vo) {
+		Categoria cat = categoriaRepository.save(new Categoria(null, vo.getNome(), vo.getDescricao()));
+		return new CategoriaDTO(cat);
+
+	}
+
+	@Transactional
+	public CategoriaDTO update(final Long id, final CategoriaVO vo) {
+		Categoria cat = findById(id);
+		cat.setNome(vo.getNome());
+		cat.setDescricao(vo.getDescricao());
+		return new CategoriaDTO(cat);
 
 	}
 
@@ -33,6 +40,16 @@ public class CategoriaService {
 	public List<CategoriaDTO> findAll() {
 		List<Categoria> cat = categoriaRepository.findAll();
 		return cat.stream().map(x -> new CategoriaDTO(x)).collect(Collectors.toList());
+
+	}
+
+	@Transactional(readOnly = true)
+	public Categoria findById(Long id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		Categoria cat = obj//
+				.orElseThrow(() -> new ObjectNotFoundException(
+						"Objeto não encontrado! id: " + id + ", tipo " + Categoria.class.getName()));
+		return cat;
 
 	}
 
